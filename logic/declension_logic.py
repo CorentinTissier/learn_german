a0b8c3d245e66850014ae4c2cb2429588c653444
CorentinTissier <corentin.tissier@outlook.fr>
2024-08-23 00:02:09 +0200

    small logic refactoring

--- logic/declension_logic.py
+++ logic/declension_logic.py
@@ -2,11 +2,56 @@ import pandas as pd
 from pathlib import Path
 from enum import Enum
 
+
+# Base directory for CSV files 
+TABLE_DIR = Path("data", "csv", "tables")
+
+
+class Gender(Enum):
+    MASCULINE = 'Masculine'
+    FEMININE = 'Feminine'
+    NEUTER = 'Neuter'
+
+
+class Case(Enum):
+    NOMINATIVE = 'Nominative'
+    ACCUSATIVE = 'Accusative'
+    DATIVE = 'Dative'
+    GENITIVE = 'Genitive'
+
+
 class TableName(Enum):
-    definite_article: 
-def load_table(table_name: str):
-def check_declension(table_name, table_root_path, determinant, case, answer):
-    table = pd.read_csv(Path(table_root_path, table_name))
-    table = table.set_index("case")
-    correct_form = declension_rules.get(determinant, {}).get(case)
+    DEFINITE_ARTICLE = 'definite article'
+    INDEFINITE_ARTICLE = 'indefinite article'
+
+def convert_name_to_file(table_name: TableName) -> str: 
+    table_file = table_name.value.lower().replace(" ", "_") + ".csv"
+    return table_file
+
+
+def load_table(table_name: TableName):
+    table_file = convert_name_to_file(table_name)
+    table_path = TABLE_DIR / table_file
+    
+    # Load the table
+    try:
+        table = pd.read_csv(table_path)
+
+    except FileNotFoundError:
+        raise FileNotFoundError(f"Error: The file {table_path} was not found.")
+    
+    except Exception as e:
+        raise RuntimeError(f"An error occurred while loading the table: {e}")
+    
+    table = table.set_index("Case")
+    
+    return table
+
+
+def check_declension(
+        table: pd.DataFrame, 
+        gender: Gender, 
+        case: Case, 
+        answer: str) -> bool:
+    correct_form = table.loc[case.value, gender.value]
     return correct_form == answer
